6824bac679433fbfd50144a27b8da091282745dd
Manpreet Singh <manpreetsingh@Manpreets-MacBook-Air.local>
2022-04-27 11:30:18 +0530

    create combineReducers

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,1 +1,7 @@
 //in this file, if we have multiple reducer, we need to combined all reducers here
+import { combineReducers } from "redux";
+import { productReducer } from "./productReducer";
+
+const reducers = combineReducers({
+  allProducts: productReducer, //set a key for allReducers like allProducts
+});
