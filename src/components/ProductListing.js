fb602009dd3ed22ffb2d48397429912f94b1393e
Manpreet Singh <manpreetsingh@Manpreets-MacBook-Air.local>
2022-04-28 09:42:15 +0530

    api call

--- src/components/ProductListing.js
+++ src/components/ProductListing.js
@@ -1,11 +1,26 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useSelector } from "react-redux";
 import Product from "./Product";
 import "./common.css";
+import axios from "axios";
 
 export const ProductListing = () => {
   const products = useSelector((state) => state); //fetch the state from redux
   console.log(products);
+
+  const fetchProducts = async () => {
+    const response = await axios
+      .get("https://fakestoreapi.com/products")
+      .catch((error) => {
+        console.log("Error", error);
+      });
+    console.log(response.data);
+  };
+
+  useEffect(() => {
+    fetchProducts();
+  }, []);
+
   return (
     <div className="ui grid container ">
       <div className="product">
