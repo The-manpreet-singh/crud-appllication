2afa9635d5f46a784a6ffebfe5ac7b8c87da9f46
Manpreet Singh <manpreetsingh@Manpreets-MacBook-Air.local>
2022-04-29 10:44:56 +0530

    show product detils

--- src/components/ProductDetail.js
+++ src/components/ProductDetail.js
@@ -6,6 +6,7 @@ import { selectedProducts } from "../redux/actions/productActions";
 
 export const ProductDetail = () => {
   const product = useSelector((state) => state.product); //its give the state access
+  const { image, title, price, category, description } = product;
   const { productId } = useParams();
   const dispatch = useDispatch();
   //console.log(productId);
@@ -25,5 +26,36 @@ export const ProductDetail = () => {
       fetchProductDetail();
     }
   }, [productId]);
-  return <div>ProductDetail</div>;
+  return (
+    <div className="ui grid container">
+      {Object.keys(product).length === 0 ? (
+        <div>...Loading</div>
+      ) : (
+        <div className="ui placeholder segment">
+          <div className="ui two column stackable center aligned grid">
+            <div className="ui vertical divider">AND</div>
+            <div className="middle aligned row">
+              <div className="column lp">
+                <img className="ui fluid image" src={image} />
+              </div>
+              <div className="column rp">
+                <h1>{title}</h1>
+                <h2>
+                  <a className="ui teal tag label">${price}</a>
+                </h2>
+                <h3 className="ui brown block header">{category}</h3>
+                <p>{description}</p>
+                <div className="ui vertical animated button" tabIndex="0">
+                  <div className="hidden content">
+                    <i className="shop icon"></i>
+                  </div>
+                  <div className="visible content">Add to Cart</div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      )}
+    </div>
+  );
 };
