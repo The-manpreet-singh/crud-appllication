300377b90c85e541917c7334283c264a2db014cc
Manpreet Singh <manpreetsingh@Manpreets-MacBook-Air.local>
2022-05-04 09:52:09 +0530

    add navbar

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,14 @@
 import React from "react";
-
+import Navbar from "./Navbar";
 export const Header = () => {
   return (
-    <div className="ui fixed menu">
-      <div className="ui container center">
-        <h1>Shop</h1>
-      </div>
+    <div>
+      <Navbar />
     </div>
+    // <div className="ui fixed menu">
+    //   <div className="ui container center">
+    //     <h1>Shop</h1>
+    //   </div>
+    // </div>
   );
 };
